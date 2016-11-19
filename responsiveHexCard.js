7653debf79dffcd774b75cb19ad6915ab8b91043
Christopher Caldwell <clc523@gmail.com>
2016-11-19 08:04:37 -0700

    Adding responsive functionality

---
+++ responsiveHexCard.js
@@ -0,0 +1,54 @@
+function respHexCard(hexClass, width, backColor, frontColor){
+  var shapeId = hexClass.slice(13, hexClass.length);
+
+  var classNames = {
+    hexCard : "hex-card" + shapeId,
+    frontSide : "front" + shapeId,
+    backSide : "back" + shapeId
+  };
+
+  $('.' + hexClass).append('<div id=' + classNames.hexCard + "><figure class=" + classNames.frontSide + "></figure><figure class=" + classNames.backSide + "></figure></div>").css({
+    "height" : "100%",
+    "width" : width,
+    "perspective" : "800px",
+    "-webkit-clip-path" : "polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%)",
+    "position" : "relative",
+    "display" : "inline-block"
+  });
+
+  $('#' + classNames.hexCard).css({
+    "height" : "100%",
+    "width" : "100%",
+    "-webkit-clip-path" : "inherit",
+    "transform-style" : "preserve-3d",
+    "transition" : "transform 2s",
+    "position" : "absolute"
+  });
+
+  $('#' + classNames.hexCard + ' figure').css({
+    "margin": "0",
+    "position": "absolute",
+    "-webkit-clip-path": "inherit",
+    "width": "100%",
+    "height": "100%",
+    "backface-visibility": "hidden",
+    "display":"block"
+  });
+
+  $('#' + classNames.hexCard + ' .' + classNames.frontSide).css({
+    "height":"100%",
+    "width":"100%",
+    "-webkit-clip-path":"inherit",
+    "background-color": frontColor,
+    "position": "absolute"
+  });
+
+  $('#' + classNames.hexCard + ' .' + classNames.backSide).css({
+    "height":"100%",
+    "width":"100%",
+    "-webkit-clip-path":"inherit",
+    "background-color": backColor,
+    "position": "absolute",
+    "transform" : "rotateY(180deg)"
+  })
+}
